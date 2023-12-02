0e26c6a07d64ef4aeeabb78ee59198ecfc215116
WhyWhat0 <108304655+WhyWhat0@users.noreply.github.com>
2023-12-02 20:31:37 +0300

    .

--- src/components/PostCreateForm.vue
+++ src/components/PostCreateForm.vue
@@ -5,12 +5,17 @@
         </div>
         <div class="chat-footer-text-area" style="margin-left: 10px;">
             <textarea
+                id="ta"
                 @submit.prevent
                 :value="currentMessage"
                 @input="updateCurrentMessage"
+                @mouseup="calculateHeight"
+                @keyup="calculateHeight"
+                @onmouseup="calculateHeight"
+                @onkeyup="calculateHeight"
                 class="message-text"
                 rows="1"
-                style="font-size: 16px;"
+                style="font-size: 16px; line-height: 16px;"
                 placeholder="Write a message..."
                 type="text"
                 @keydown.enter.prevent.exact="createMessage"
@@ -40,6 +45,13 @@ export default {
                 date: '',
                 files: '',
                 isMyMessage: true,
+                handlers: {
+                    mouseup: this.calculateHeight,
+                    keyup: this.calculateHeight,
+                    onmouseup: this.calculateHeight,
+                    onkeyup: this.calculateHeight,
+
+                }
             },
         }
     },
@@ -47,6 +59,9 @@ export default {
         ...mapMutations({
             setCurrentMessage: 'setCurrentMessage',
         }),
+        mounted() {
+            this.calculateHeight();
+        },
         updateCurrentMessage(e) {
             this.$store.commit('setCurrentMessage', e.target.value)
         },
@@ -68,13 +83,51 @@ export default {
             let caret = e.target.selectionStart;
             e.target.setRangeText("\n", caret, caret, "end");
             this.message.text = e.target.value;
-        }
+        },
+        calculateContentHeight(ta, scanAmount) {
+            var origHeight = ta.style.height,
+                height = ta.offsetHeight,
+                scrollHeight = ta.scrollHeight,
+                overflow = ta.style.overflow;
+            if (height >= scrollHeight) {
+                ta.style.height = (height + scanAmount) + 'px';
+                ta.style.overflow = 'hidden';
+                if (scrollHeight < ta.scrollHeight) {
+                    while (ta.offsetHeight >= ta.scrollHeight) {
+                        ta.style.height = (height -= scanAmount) + 'px';
+                    }
+                    while (ta.offsetHeight < ta.scrollHeight) {
+                        ta.style.height = (height++) + 'px';
+                    }
+                    ta.style.height = origHeight;
+                    ta.style.overflow = overflow;
+                    return height;
+                }
+            } else {
+                return scrollHeight;
+            }
+        },
+        calculateHeight() {
+            var ta = document.getElementById("ta"),
+                style = (window.getComputedStyle) ?
+                    window.getComputedStyle(ta) : ta.currentStyle,
+                taLineHeight = parseInt(style.lineHeight, 10),
+                taHeight = this.calculateContentHeight(ta, taLineHeight),
+                numberOfLines = Math.ceil(taHeight / taLineHeight) - 1;
+
+            console.log(numberOfLines)
+        },
     },
     computed: {
         ...mapState({
             currentMessage: state => state.currentMessage,
         })
     },
+    // watch: {
+    //     currentMessage() {
+    //         this.calculateHeight()
+    //     }
+    // }
 }
 </script>
 <style scoped>
