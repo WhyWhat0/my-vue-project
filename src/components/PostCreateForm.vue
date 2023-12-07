81ff329535152880c95959441b121e1edb63f31d
WhyWhat0 <108304655+WhyWhat0@users.noreply.github.com>
2023-12-07 15:07:09 +0300

    похуй

--- src/components/PostCreateForm.vue
+++ src/components/PostCreateForm.vue
@@ -3,7 +3,7 @@
         <div class="chat-footer-item">
             <i class="fa fa-paperclip fa-flip-horizontal fa-fw" aria-hidden="true" style="font-size: 25px;"></i>
         </div>
-        <div class="chat-footer-text-area" style="margin-left: 10px;">
+        <div class="chat-footer-text-area grow-wrap" style="margin-left: 10px;">
             <textarea
                 v-focus
                 id="ta"
@@ -94,8 +94,11 @@ export default {
         },
         changeDivArea() {
             ta.rows = this.sizeFooterArea
-            this.$store.commit('setChatBodyHeight', 84 - 2 * (this.sizeFooterArea - 1))
-            this.$store.commit('setChatFooterHeight', 8 + 2 * (this.sizeFooterArea - 1))
+            if (this.sizeFooterArea < 10) {
+                this.$store.commit('setChatBodyHeight', 84 - 2 * (this.sizeFooterArea - 1))
+                this.$store.commit('setChatFooterHeight', 8 + 2 * (this.sizeFooterArea - 1))
+            }
+
         }
 
     },
@@ -122,5 +125,49 @@ export default {
 </script>
 <style scoped>  .fa {
       font-size: small;
+  }
+
+  .grow-wrap {
+      /* easy way to plop the elements on top of each other and have them both sized based on the tallest one's height */
+      display: grid;
+  }
+
+  .grow-wrap::after {
+      /* Note the weird space! Needed to preventy jumpy behavior */
+      content: attr(data-replicated-value) " ";
+
+      /* This is how textarea text behaves */
+      white-space: pre-wrap;
+
+      /* Hidden from view, clicks, and screen readers */
+      visibility: hidden;
+  }
+
+  .grow-wrap>textarea {
+      /* You could leave this, but after a user resizes, then it ruins the auto sizing */
+      resize: none;
+
+      /* Firefox shows scrollbar on growth, you can hide like this. */
+      overflow: hidden;
+  }
+
+  .grow-wrap>textarea,
+  .grow-wrap::after {
+      /* Identical styling required!! */
+      /* border: 1px solid black; */
+      padding: 0.5rem;
+      font: inherit;
+
+      /* Place on top of each other */
+      grid-area: 1 / 1 / 2 / 2;
+  }
+
+  /* body {
+      margin: 2rem;
+      font: 1rem/1.4 system-ui, sans-serif;
+  } */
+
+  label {
+      display: block;
   }
 </style>
--- src/mixins/toggleMixin.js
+++ src/mixins/toggleMixin.js
@@ -29,7 +29,7 @@ export default {
                 window.getComputedStyle(ta) : ta.currentStyle,
             taLineHeight = parseInt(style.lineHeight, 10),
             taHeight = this.calculateContentHeight(ta, taLineHeight),
-            numberOfLines = Math.ceil((taHeight) / taLineHeight)-1;
+            numberOfLines = Math.ceil((taHeight) / taLineHeight)-2;
             ta.scrollHeight
             console.log(ta),
             // console.log('numberOfLines =',numberOfLines, '\n',
